4c535ecffa380a7a516acefabef93207a9255739
Ben <BforBen@users.noreply.github.com>
2016-01-20 21:21:22 +0000

    Template updates

--- scripts/sds.js
+++ scripts/sds.js
@@ -4,4 +4,16 @@ jQuery(function ($) {
         var hash = this.hash;
         $('html, body').animate({ scrollTop: $(hash).offset().top }, 300, function () { window.location.hash = hash; });
     });
+    
+    $('.landing .navbar-sds').addClass('hide');
+    
+    var prev = 0;
+    var win = $(window);
+    var nav = $('.navbar-sds');
+    
+    win.on('scroll', function(){
+        var scrollTop = win.scrollTop();
+        nav.toggleClass('hide', scrollTop > prev);
+        prev = scrollTop;
+    });
 });
