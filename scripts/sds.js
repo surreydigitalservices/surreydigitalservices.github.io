25593670a80664d00fd4bc9aebdb93d20870211a
Ben <ben@cheetham.me.uk>
2016-01-20 20:43:23 +0000

    Move styles and scripts to own files

---
+++ scripts/sds.js
@@ -0,0 +1,7 @@
+jQuery(function ($) {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({ scrollTop: $(hash).offset().top }, 300, function () { window.location.hash = hash; });
+    });
+});
